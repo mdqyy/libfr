6d522d1d6b8e73faf3ea3b75ecc051133110fcbf
korczis <korczis@gmail.com>
2013-08-05 22:20:24 +0200

    Linking with boost

--- apps/camera/src/main.cpp
+++ apps/camera/src/main.cpp
@@ -2,6 +2,10 @@
 #include <cstdlib>
 #include <iostream>
 
+#include <boost/program_options/options_description.hpp>
+#include <boost/program_options/option.hpp>
+
+namespace po = boost::program_options;
 #include <opencv/cv.h>
 #include <opencv/cxcore.h>
 #include <opencv/highgui.h>
@@ -11,41 +15,48 @@ using namespace cv;
  
 int main(int argc, char** argv)
 {
-  std::cout << "Hello World! I am nice camera app!" << std::endl;
+    po::options_description desc("Allowed options");
+        desc.add_options()
+            ("help", "produce help message")
+            ;
+
+    std::cout << "Hello World! I am nice camera app!" << std::endl;
   
-  CvCapture* capture = cvCaptureFromCAM(0);
-  if ( !capture ) {
-    fprintf( stderr, "ERROR: capture is NULL \n" );
-    getchar();
-    return EXIT_FAILURE;
-  }
-  // Create a window in which the captured images will be presented
-  cvNamedWindow( "mywindow", WINDOW_AUTOSIZE );
-  // Show the image captured from the camera in the window and repeat
-  while ( 1 ) 
-  {
-    // Get one frame
-    IplImage* frame = cvQueryFrame( capture );
-    if ( !frame )
-    {
-      fprintf( stderr, "ERROR: frame is null...\n" );
-      getchar();
-      break;
+    CvCapture* capture = cvCaptureFromCAM(0);
+    if ( !capture ) {
+        fprintf( stderr, "ERROR: capture is NULL \n" );
+        getchar();
+        return EXIT_FAILURE;
     }
-  
-    cvShowImage( "mywindow", frame );
-    // Do not release the frame!
-    //If ESC key pressed, Key=0x10001B under OpenCV 0.9.7(linux version),
-    //remove higher bits using AND operator
-    if ( (cvWaitKey(10) & 255) == 27 ) 
+
+    // Create a window in which the captured images will be presented
+    cvNamedWindow( "mywindow", WINDOW_AUTOSIZE );
+
+    // Show the image captured from the camera in the window and repeat
+    while ( 1 )
     {
-      break;
+        // Get one frame
+        IplImage* frame = cvQueryFrame( capture );
+        if ( !frame )
+        {
+            fprintf( stderr, "ERROR: frame is null...\n" );
+            getchar();
+            break;
+        }
+
+        cvShowImage( "mywindow", frame );
+        // Do not release the frame!
+        //If ESC key pressed, Key=0x10001B under OpenCV 0.9.7(linux version),
+        //remove higher bits using AND operator
+        if ( (cvWaitKey(10) & 255) == 27 )
+        {
+            break;
+        }
     }
-  }
-  
-  // Release the capture device housekeeping
-  cvReleaseCapture( &capture );
-  cvDestroyWindow( "mywindow" );
-  
-  return EXIT_SUCCESS;
+
+    // Release the capture device housekeeping
+    cvReleaseCapture( &capture );
+    cvDestroyWindow( "mywindow" );
+
+    return EXIT_SUCCESS;
 }

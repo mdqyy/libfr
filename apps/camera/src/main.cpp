5b9f75cfc21d078843f650403577143b9a38508b
korczis <korczis@gmail.com>
2013-08-05 22:51:22 +0200

    Working boost program options for help message

--- apps/camera/src/main.cpp
+++ apps/camera/src/main.cpp
@@ -2,10 +2,9 @@
 #include <cstdlib>
 #include <iostream>
 
-#include <boost/program_options/options_description.hpp>
-#include <boost/program_options/option.hpp>
-
+#include <boost/program_options.hpp>
 namespace po = boost::program_options;
+
 #include <opencv/cv.h>
 #include <opencv/cxcore.h>
 #include <opencv/highgui.h>
@@ -17,8 +16,27 @@ int main(int argc, char** argv)
 {
     po::options_description desc("Allowed options");
         desc.add_options()
-            ("help", "produce help message")
+            ("help,h", "produce help message")
             ;
+
+    po::variables_map vm;
+    try
+    {
+        po::store(po::parse_command_line(argc, argv, desc), vm);
+        po::notify(vm);
+    }
+    catch(const std::exception& e)
+    {
+        std::cout << "Unable to parse program options." << std::endl;
+        std::cout << desc << std::endl;
+        return EXIT_FAILURE;
+    }
+
+    if (vm.count("help"))
+    {
+        std::cout << desc << std::endl;
+        return EXIT_SUCCESS;
+    }
 
     std::cout << "Hello World! I am nice camera app!" << std::endl;
   

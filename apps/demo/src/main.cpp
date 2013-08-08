f7ad379a1f3a7f8b11f6df5724cc0511bdab8d1a
korczis <korczis@gmail.com>
2013-08-08 03:16:31 +0200

    Basic Qt

--- apps/demo/src/main.cpp
+++ apps/demo/src/main.cpp
@@ -2,7 +2,13 @@
 #include <cstdlib>
 #include <iostream>
 
+#include <boost/program_options.hpp>
+namespace po = boost::program_options;
+
 #include <libfr.h>
+
+#include <QApplication>
+#include <QWidget>
 
 namespace fr {
 	namespace app {
@@ -20,5 +26,40 @@ namespace fr {
 
 int main(int argc, char** argv)
 {
+    // Specify program options
+    po::options_description desc("Allowed options");
+        desc.add_options()
+            ("help,h", "produce help message")
+            ("gui,g", "show gui")
+            ;
+
+    po::variables_map vm;
+
+    // Try to parse program options
+    try
+    {
+        po::store(po::parse_command_line(argc, argv, desc), vm);
+        po::notify(vm);
+    }
+    catch(const std::exception& e)
+    {
+        std::cout << "Unable to parse program options, reason: " << e.what() << std::endl;
+        std::cout << desc << std::endl;
+        return EXIT_FAILURE;
+    }
+
+    if(vm.count("gui"))
+    {
+        QApplication app(argc, argv);
+
+        QWidget window;
+
+        window.resize(250, 150);
+        window.setWindowTitle("Simple example");
+        window.show();
+
+        return app.exec();
+    }
+
     return fr::app::AppDemo().Run(argc, argv);
 }
